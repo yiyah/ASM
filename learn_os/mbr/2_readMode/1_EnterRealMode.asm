f20e62aba74862fbe20c2c94acfaf6a56972e013
yiyah <756307810@qq.com>
2022-01-05 23:20:54 +0800

    :tada: Add: Add the code of enter real mode
    [Description]
    Add the code of enter real mode

---
+++ learn_os/mbr/2_readMode/1_EnterRealMode.asm
@@ -0,0 +1,78 @@
+%include "pm.inc"
+
+        org     0x7C00
+        jmp     LABEL_BEGIN
+
+[SECTION .gdt]
+;                                  base,    segment limit, segment attribute
+LABEL_GDT:          Descriptor        0,                0, 0
+LABEL_DESC_CODE32:  Descriptor        0, SegCode32Len - 1, DA_C + DA_32
+LABEL_DESC_VIDEO:   Descriptor  0xB8000,           0xFFFF, DA_DRW
+
+GDTLEN          equ     $ - LABEL_GDT   ; length of GDT
+GDTPTR          dw      GDTLEN - 1      ; limit of GDT
+                dd      0               ; segment base address of GDT
+
+; GDT selector
+SelectorCode32      equ     LABEL_DESC_CODE32 - LABEL_GDT
+SelectorVideo       equ     LABEL_DESC_VIDEO  - LABEL_GDT
+; END of [SECTION .gdt]
+
+[SECTION .s16]
+[BITS   16]
+LABEL_BEGIN:
+        mov     ax,cs
+        mov     ss,ax
+        mov     sp,0x100
+
+        ; init Descriptor
+        xor     eax,eax
+        mov     ax,cs
+        shl     eax,4
+        add     eax,LABEL_SEG_CODE32
+        mov     [LABEL_DESC_CODE32+2],ax
+        shr     eax,16
+        mov     [LABEL_DESC_CODE32+4],al
+        mov     [LABEL_DESC_CODE32+7],ah
+
+        ; calculate gdtr
+        xor     eax,eax
+        mov     ax,cs
+        shl     eax,4
+        add     eax,LABEL_GDT
+        mov     [GDTPTR+2],eax
+        lgdt    [GDTPTR]
+
+        ; close IRQ
+        cli
+
+        ; open A20
+        in      al,92H
+        or      al,00000010B
+        out     92H,al
+
+        ; set the protect flag
+        mov     eax,cr0
+        or      eax,1
+        mov     cr0,eax
+
+        ; enter protect mode
+        jmp     dword SelectorCode32:0
+
+; END of [SECTION .s16]
+
+[SECTION .s32]
+[BITS   32]
+LABEL_SEG_CODE32:
+        ; show something
+        mov     ax,SelectorVideo
+        mov     es,ax
+        mov     edi,80*2*10+79*2
+        
+        mov     al,'P'
+        mov     ah,02H
+        mov     [es:edi],ax
+        jmp     $
+
+SegCode32Len    equ     $ - LABEL_SEG_CODE32
+; END of [SECTION .s32]
---
+++ learn_os/mbr/2_readMode/readme.md
@@ -0,0 +1,20 @@
+# Real mode
+
+## Abbreviate
+
+GDT: Global Description Table
+GDTR: Global Description Table Register
+
+## steps for enter real mode
+
+step1: Prepare the GDT
+step2: Prepare the selector
+step3: Init the GDT(descriptor)
+step4: Calculate the segment base address of GDT and load GDTR(lgdt)
+step5: Close the interruption(cli)
+step6: Open the address line A20 and set the flag of protect mode(cr0)
+step7: Enter protect mode
+
+## the result of 1_EnterRealMode.asm
+
+![看不到图片是科学问题](https://raw.githubusercontent.com/yiyah/Picture_Material/master/20220105231546.png)
