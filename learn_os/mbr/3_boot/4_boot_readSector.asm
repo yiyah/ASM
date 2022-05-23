1595d28fd7632a5b33b38d1c53137413ab80d6b8
yiyah <756307810@qq.com>
2022-05-23 22:30:51 +0800

    :sparkles: Feat: add ReadSector
    [Description]
    Add ReadSector

--- learn_os/mbr/3_boot/4_boot_readSector.asm
+++ learn_os/mbr/3_boot/4_boot_readSector.asm
@@ -57,5 +57,54 @@ Disp_ret:
     pop     es
     ret
 
+; ===================================
+; @Function: ReadSector(dw startSector,db num)
+; @Brief: Read num sectors starting from the startSector sector
+;         -------------------------------------------------------------
+;         扇区号求扇区在磁盘中的位置 (扇区号 -> 柱面号, 起始扇区, 磁头号) (LBA -> CHS)
+;         -------------------------------------------------------------
+;         设start扇区号为 x
+;                                  ┌ 柱面号 = y >> 1
+;               x           ┌ 商 y ┤
+;         -------------- => ┤      └ 磁头号 = y & 1
+;          每磁道扇区数       │
+;                           └ 余 z => 起始扇区号 = z + 1
+; @param: [IN] startSector
+; @param: [IN] num
+; @stack: bp, ip, startSector, num
+; ===================================
+ReadSector:
+    push	bp
+    mov	    bp, sp
+    push    ax    
+    push    cx
+    push    dx
+
+    mov	    dl, [BPB_SecPerTrk] ; bl: 除数
+    mov     ax, [bp+4]
+    div	    dl                  ; y 在 al 中, z 在 ah 中
+    inc	    ah                  ; z++
+    mov	    cl, ah              ; cl <- 起始扇区号
+    mov	    dh, al              ; dh <- y
+    shr	    al, 1               ; y >> 1 (y/BPB_NumHeads)
+    mov	    ch, al              ; ch <- 柱面号
+    and	    dh, 1               ; dh & 1 = 磁头号
+
+    ; 至此, "柱面号, 起始扇区, 磁头号" 全部得到
+    mov	    dl, [BS_DrvNum]		; 驱动器号 (0 表示 A 盘)
+_GoOnReading:
+    mov	    ah, 2			    ; 读
+    mov	    al, byte [bp+6]		; 读 al 个扇区
+    int	    13h
+    jc	    _GoOnReading		; 如果读取错误 CF 会被置为 1, 
+    				            ; 这时就不停地读, 直到正确为止
+    pop     dx
+    pop     cx
+    pop     ax
+    mov     sp, bp
+    pop	    bp
+
+    ret
+
 times   510-($-$$) db 0
 dw      0xAA55
